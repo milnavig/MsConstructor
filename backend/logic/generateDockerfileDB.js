9fcc298fd6ccb319438cf3a1074d1a4c28e3c9b8
milnavig <milnavig@gmail.com>
2022-04-15 21:47:14 +0300

    small fix

--- backend/logic/generateDockerfileDB.js
+++ backend/logic/generateDockerfileDB.js
@@ -1,7 +1,7 @@
 module.exports = function generateDockerfileDB(dbName) {
   const dockerfile = 
 `FROM postgres:10.5
-ADD db/${dbName}/script.sql /docker-entrypoint-initdb.d
+ADD /script.sql /docker-entrypoint-initdb.d
 RUN chmod a+r /docker-entrypoint-initdb.d/*
 EXPOSE 5432
 `;
